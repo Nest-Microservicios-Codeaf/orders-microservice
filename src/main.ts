17079c17f7aa0696f6097827e2a3cbc850bc8ce3
DaniSW15 <daniel.cg15.tab@gmail.com>
2025-12-08 14:49:17 -0600

    feat: initialize orders microservice with NestJS and Prisma
    - Added package.json for project dependencies and scripts.
    - Created prisma.config.ts for Prisma configuration.
    - Added initial migration for Order model with status enum.
    - Defined Prisma schema with Order model and OrderStatus enum.
    - Set up main application module and configuration for environment variables.
    - Implemented Orders module with controller and service for order management.
    - Created DTOs for creating and updating orders.
    - Added basic e2e tests for application.
    - Configured TypeScript settings for building the project.

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const logger = new Logger('OrderMS-Main');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      port: envs.port,
+    },
+  });
+  
+  await app.listen();
+
+  logger.log(`🚀 Application is running on: http://localhost:${envs.port}`);
+}
+bootstrap();
